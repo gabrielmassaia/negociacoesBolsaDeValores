2e58ad0914f4682e0cca0a13708fee8739952310
Gabriel Massaia <gabrielmassaia50@gmail.com>
2024-04-10 18:15:36 -0300

    deixei a negociação imutavel

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,23 +1,24 @@
 class Negociacao {
     constructor (data, quantidade, valor) {
-        this._data = data;
+        this._data = new Date(data.getTime())
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this)
     }
 
-    getData() {
-        return this._data
+    get data() {
+        return new Date(this._data.getTime())
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade
     }
 
-    getValor() {
+    get valor() {
         return this._valor
     }
 
-    getVolume() {
+    get volume() {
         return this._quantidade * this._valor
     }
 }
