b3863e84155b54e892751358920e24f8ead94fb6
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2024-04-13 16:43:38 -0300

    commit da negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,8 +12,19 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','))
-        console.log(data)
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
         
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao)
+
     }
 }
