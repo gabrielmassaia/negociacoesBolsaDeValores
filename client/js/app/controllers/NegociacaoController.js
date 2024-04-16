beaa88dfa1a75647fefbdad29c013ff80e36b7ee
Gabriel Massaia <gabrielmassaia50@gmail.com>
2024-04-16 18:08:39 -0300

    Isolei as responsabilidades

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,19 +12,13 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-        
-        let negociacao = new Negociacao(
-            data,
+        let negociacao = new Negociacao (
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao)
-
+        console.log(DateHelper.dataParaTexto(negociacao.data))
     }
 }
